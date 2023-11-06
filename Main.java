c1f7b6b5861e49d5119421f02c56c67864f7b6a4
David Yin <29613693+E137299@users.noreply.github.com>
2023-11-06 08:20:58 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 class Main {
     public static void main(String[] args) {
-      
+      //Creates a Shopcart from the ShoppingCart class
+
+      // Create a Scanner object
+
+      // Create a Scanner object
+
+      /*
+      Repeatedly asks user what they want to do:
+      - 'Check Out' - If user chooses 'Check Out', the program will stop, print content of cart and total price.
+      - 'Buy' - Prompts user for information(name, price and quantity) about the item that they want to purchase, adds item to cart, prints content of cart
+      - 'Remove' - Prompts user for the item and quantity that they want to remove, removes item and quantity, prints content of cart
+
+      */
+
+
     }
   }
